c354d59072722e203cedcc8dcc209b27d8f9ec5d
buliang <2450980241@qq.com>
2021-02-01 11:23:21 +0800

    次月复习计算，完成复习新布局

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -7,26 +7,21 @@
 					<el-button type="primary" round @click="modifyClick">修改记录</el-button>
 					<el-button type="primary" round @click="deleteClick">删除记录</el-button>
 					<el-button type="primary" round @click="timeClick">计时</el-button>
+					<el-button type="primary" round @click="isFinished">完成复习</el-button>
 				</div>
 			</el-aside>
 			<el-container>
 				<el-main>
 					<el-table :data="tableData"  style="width: 100%;" @row-click="rowClick" :highlight-current-row="true" :row-class-name="tableRowClassName">
-						<el-table-column type="index" width="50">
+						<el-table-column type="index" width="50" >
 						</el-table-column>
 						<el-table-column prop="day" label="日期" width="120">
 						</el-table-column>
-						<el-table-column prop="study" label="学习内容" width="230">
+						<el-table-column prop="study" label="学习内容" width="440">
 						</el-table-column>
 						<el-table-column prop="time" label="学习时长(分钟)">
 						</el-table-column>
-						<el-table-column prop="review" label="复习" width="180">
-						</el-table-column>
-						<el-table-column label="操作" >
-							<template >
-								<el-button size="mini" @click="isFinished">完成</el-button>
-								
-							</template>
+						<el-table-column prop="review" label="复习" width="200">
 						</el-table-column>
 					</el-table>
 				</el-main>
@@ -102,7 +97,7 @@
 			//复习完成与否操作
 			isFinished(){
 				if(this.currentRow===""){
-					this.$message.error('请先选择本条记录');
+					this.$message.error('请先选择一条记录');
 				}else{
 					this.$message.success('已完成');
 					let data = JSON.parse(localStorage.getItem('data'))
@@ -110,6 +105,7 @@
 					localStorage.setItem("data",JSON.stringify(data))
 					this.getData(this.currentPage)
 				}
+				this.currentRow=""
 			},
 			//计时
 			timeClick(){
@@ -180,18 +176,42 @@
 					
 					//添加一个用于计算时长的id，用来data中定位
 					this.tableData[i].tid = i 
-					
-					if(i===0){
-						this.tableData[i].review=[]
-					}
-					else if(i===1){
-						this.tableData[i].review=[1]
-					}else if(i>=2 && i<=3){
-						this.tableData[i].review=[" ",i," ",i-1]
-					}else if(i>=4 && i<=6){
-						this.tableData[i].review=[" ",i," ",i-1," ",i-3]
+					//计算该复习的day
+					if(n===1){
+						if(i===0){
+							this.tableData[i].review=[]
+						}
+						else if(i===1){
+							this.tableData[i].review=[1]
+						}else if(i>=2 && i<=3){
+							this.tableData[i].review=[" ",i," ",i-1]
+						}else if(i>=4 && i<=6){
+							this.tableData[i].review=[" ",i," ",i-1," ",i-3]
+						}else{
+							this.tableData[i].review=[" ",i," ",i-1," ",i-3," ",i-6]
+						}
 					}else{
-						this.tableData[i].review=[" ",i," ",i-1," ",i-3," ",i-6]
+						const bmc = JSON.parse(localStorage.getItem('data'))[n-2].children.length
+						
+						if(i===0){
+							this.tableData[i].review=[" ",bmc <=0 ? " ":-bmc," ",bmc-1 <=0 ? " ":-(bmc-1)," ",bmc-3 <=0 ? " ":-(bmc-3)," ",bmc-6 <=0 ? " ":-(bmc-6)]
+						}
+						else if(i===1){
+							this.tableData[i].review=[" ",i," ",bmc <=0 ? " ":-bmc," ",bmc-2 <=0 ? " ":-(bmc-2)," ",bmc-5 <=0 ? " ":-(bmc-5)]
+						}else if(i===2){
+							this.tableData[i].review=[" ",i," ",i-1," ",bmc-1 <=0 ? " ":-(bmc-1)," ",bmc-4 <=0 ? " ":-(bmc-4)]
+						}else if(i===3){
+							this.tableData[i].review=[" ",i," ",i-1," ",bmc <=0 ? " ":-bmc," ",bmc-3 <=0 ? " ":-(bmc-3)]
+						}else if(i===4){
+							this.tableData[i].review=[" ",i," ",i-1," ",i-3," ",bmc-2 <=0 ? " ":-(bmc-2)]
+						}else if(i===5){
+							this.tableData[i].review=[" ",i," ",i-1," ",i-3," ",bmc-1 <=0 ? " ":-(bmc-1)]
+						}else if(i===6){
+							this.tableData[i].review=[" ",i," ",i-1," ",i-3," ",bmc <=0 ? " ":-bmc]
+						}else{
+							this.tableData[i].review=[" ",i," ",i-1," ",i-3," ",i-6]
+						}
+						
 					}
 				}
 			}
@@ -209,7 +229,7 @@
 			justify-content space-around
 			align-items center
 			.el-button
-				margin 23px
+				margin 18px
 				width 150px
 				height 50px
 				font-size 20px
