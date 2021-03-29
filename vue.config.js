4f3cced4bbb0c7eb1ba71a9410ca07e4d2f253d6
buliang <2450980241@qq.com>
2021-03-29 18:33:34 +0800

    优化第一次提交，图片压缩，gzip

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
+
 module.exports = {
 	publicPath: './' ,
 	//多页打包配置
@@ -19,6 +22,30 @@ module.exports = {
 			filename: 'login.html',
 			title: 'Login Page'
 		}
+	},
+	productionSourceMap: false,
+	chainWebpack: config => {
+        config.module
+            .rule('images')
+            .use('image-webpack-loader')
+            .loader('image-webpack-loader')
+            .options({ bypassOnDebug: true })
+            .end()
+    },
+	configureWebpack: config => {
+		config.plugins.push(
+	      new CompressionWebpackPlugin({
+	          filename: info => {
+	            return `${info.path}.gz${info.query}`
+	          },
+	          algorithm: 'gzip',
+	          threshold: 1024, // 只有大小大于该值的资源会被处理 10240
+	          test: new RegExp('\\.(' + ['js'].join('|') + ')$'
+	          ),
+	          minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+	          deleteOriginalAssets: false // 删除原文件
+	      })
+	    )
 	},
 	
 	css: {
